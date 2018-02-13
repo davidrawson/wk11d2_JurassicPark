80f154731c46afc7fc36583a4751da97b401de11
David Rawson <dm.rawson@gmail.com>
2018-02-13 20:15:39 +0000

    MVP done.

--- park.js
+++ park.js
@@ -13,22 +13,35 @@ Park.prototype.addDinosaur = function(dinosaur){
 Park.prototype.removeType = function(type){
   const toBeRemoved = [];
   for (let i = 0; i < this.enclosure.length; i += 1){
-    console.log(this.enclosure[i].type);
     if (this.enclosure[i].type === type){
       toBeRemoved.push(i);
     }
   }
-  for (let i = toBeRemoved.length; i >= 0; i -= 1){
-    index = toBeRemoved[i];
+  for (let i = toBeRemoved.length; i > 0; i -= 1){
+    index = toBeRemoved[i-1];
     this.enclosure.splice(index, 1);
   }
-
-  console.log(this.enclosure);
-  console.log(toBeRemoved);
-  // for (dinosaur of enclosure){
-  // this.enclosure.filter(dinosaur.type === type);
-
+  // console.log('remove by type',this.enclosure);
 }
 
+Park.prototype.overTwoOffspring = function(){
+  const toBeReturned = [];
+  for (let i = 0; i < this.enclosure.length; i += 1){
+    if (this.enclosure[i].numberOfOffspring > 2){
+      toBeReturned.push(i);
+    }
+  }
+
+  const returningDinosaurs = [];
+
+  for (let i = toBeReturned.length; i > 0; i -= 1){
+    index = toBeReturned[i-1];
+    let dino = this.enclosure.splice(index, 1)
+    returningDinosaurs.push(dino);
+  }
+  // console.log('enclosure', this.enclosure);
+  // console.log('returning Dinos', returningDinosaurs);
+  return returningDinosaurs;
+}
 
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -10,9 +10,9 @@ describe ('Park', function(){
   let park;
 
   beforeEach(function(){
-    dinosaur1 = new Dinosaur("Tyrannosaurus", 3);
+    dinosaur1 = new Dinosaur("Tyrannosaurus", 1);
     dinosaur2 = new Dinosaur("Velociraptor", 9);
-    dinosaur3 = new Dinosaur("Triceratops", 5);
+    dinosaur3 = new Dinosaur("Triceratops", 2);
     dinosaur4 = new Dinosaur("Velociraptor", 8);
 
     park = new Park;
@@ -31,10 +31,26 @@ describe ('Park', function(){
 
   it('should be able to remove all dinosaurs of a particular type', function(){
     park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur2);
     park.addDinosaur(dinosaur3);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur4);
     park.addDinosaur(dinosaur4);
     park.removeType(dinosaur2.type);
     const actual = park.numberOfDinosaurs();
-    assert.strictEqual(actual, 1);
-  })
+    assert.strictEqual(actual, 2);
+  });
+
+  it('should get all the dinosaurs with an offspring count of more than 2', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    park.addDinosaur(dinosaur3);
+    park.addDinosaur(dinosaur4);
+    const actual = park.overTwoOffspring().length;
+    assert.strictEqual(actual, 2);
+  });
 });
